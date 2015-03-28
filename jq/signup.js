05d6757f33554075a274c50c8ebdee716330882d
rocduran <rocduran@gmail.com>
2015-03-28 18:38:15 +0100

    Afegit validació de cognom.

--- jq/signup.js
+++ jq/signup.js
@@ -1,92 +1,101 @@
 $(document).ready(function(){
-    
-  //variables
-   var name = $("#nom");
-       email = $("#email");
-       comment = $(".palabras textarea");
-       modal_name = $(".modal span");
-       modal = $(".modal");
-  
-   //hide modal
-    modal.hide();
 
-   //check if name is empty
-   name.on("blur",function(){
+  //variables
+  var nom = $("#nom");
+  cognom = $("#cognom");
+  email = $("#email");
+  comment = $(".palabras textarea");
+
+
+   //Comprova si el NOM esta buit
+   nom.on("blur",function(){
     var $this = $(this);
     if($this.val().length === 0 ||
-       $this.val() === "Nom"){
-        $this.addClass("noValid");
-        $this.val("");
-    } else{
-        $this.addClass("valid");
-      }
-   });
-    
-    //check if email is empty
-  email.on("blur",function(){
-    var $this = $(this);
-    if($this.val().length === 0 ||
-       $this.val() === "Email"){
+     $this.val() === "Nom"){
       $this.addClass("noValid");
-        }
+    $this.val("");
+  } else{
+    $this.addClass("valid");
+  }
+});
+
+   cognom.on("blur",function(){
+    var $this = $(this);
+    if($this.val().length === 0 ||
+     $this.val() === "Nom"){
+      $this.addClass("noValid");
+    $this.val("");
+  } else{
+    $this.addClass("valid");
+  }
+});
+
+
+    //Comprova si el EMAIL esta buit
+    email.on("blur",function(){
+      var $this = $(this);
+      if($this.val().length === 0 ||
+       $this.val() === "Email"){
+        $this.addClass("noValid");
+    }
   });
     
-    //check if email is valid
-  email.on("blur",function(){
-    var $this = $(this);
-    if($this.val().indexOf("." && "@") > -1){
+    //Comprova si l'EMAIL es valid
+    email.on("blur",function(){
+      var $this = $(this);
+      if($this.val().indexOf("." && "@") > -1){
        $this.addClass("valid");
-    } else{
-        $this.addClass("noValid");
-        $this.val("Email");
+     } else{
+      $this.addClass("noValid");
+      $this.val("Email");
     }
   });
     
     //check if textarea is empty
-  comment.on("blur",function(){
-    var $this = $(this);
-    if($this.val() === ""){
-      $this.addClass("noValid");
-      $this.val("Comments");
-    } else{
-      $this.addClass("valid");
-    }
- });
+    comment.on("blur",function(){
+      var $this = $(this);
+      if($this.val() === ""){
+        $this.addClass("noValid");
+        $this.val("Comments");
+      } else{
+        $this.addClass("valid");
+      }
+    });
     
     //clear inputs on click
     $(".input").on("focus",function(){
-        $(this).val("");
+      $(this).val("");
     });
     
     //show modal when inputs are valid and button
     //is clicked
-  $(".submit").on("click",function(){
-    if(name.val() !== "Name" && name.val() !== "" &&
+    $(".submit").on("click",function(){
+      if(name.val() !== "Name" && name.val() !== "" &&
        email.val() !== "Email" && 
        email.val() !== "" &&
        comment.val() !== "Comments" && 
        comment.val()!== ""){
 
           //remove invalid/valid classes
-          name.removeClass().addClass("input");
-          email.removeClass().addClass("input");
-          comment.removeClass().addClass("input");
-          
+        name.removeClass().addClass("input");
+        email.removeClass().addClass("input");
+        comment.removeClass().addClass("input");
+
           //modal box
           modal_name.text(name.val());
           modal.slideDown("medium")
-            .delay(2000).slideUp("fast");
-      
+          .delay(2000).slideUp("fast");
+
           //put default text back
           name.val("Name");
           email.val("Email");
           comment.val("Comments");
-      
+
           return false;
-      
-    } else{
-      
-        return false; }
-    });
+
+        } else{
+
+          return false; }
+        });
     
-});
+  });
