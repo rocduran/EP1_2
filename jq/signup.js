6b213a244f421aa030bba69aeae58c6a1fd39b14
rocduran <rocduran@gmail.com>
2015-04-10 12:12:49 +0200

    Corretgit missatge de validació. Afegit validació de cognom, afegit combobox per CodiPostal i corretgit datepicker.

--- jq/signup.js
+++ jq/signup.js
@@ -6,32 +6,43 @@ $(document).ready(function(){
   var nom = $("#nom");
   var cognom = $("#cognom");
   var email = $("#email");
-  var inp = document.getElementsByTagName("input");
-  var comment = $(".palabras textarea");
 
+    
+  $(function() {
+    $( "#datepicker" ).datepicker(
+      {
+        dateFormat: "dd-mm-yy"
+      });
+
+  });
 
    //Comprova si el NOM esta buit
    nom.on("blur",function(){
     var $this = $(this);
-    var re = /^[A-Za-z]{2,20}$/;
-    var val = $this.val();
+    var regex = /^[A-Za-z]{2,20}$/;
+    var valor = $this.val();
 
     var errPara = document.createElement("p");
     var errText = document.createTextNode("Mínim 2 caràcters i màxim 20 caràcters");
     errPara.appendChild(errText);
 
-    if(!re.test(val)){
+    if(!regex.test(valor)){
       $this.addClass("noValid");
-      var element = document.getElementById("errNom");
-      element.appendChild(errPara);
+      var divErr = document.getElementById("errNom");
+      if (divErr.firstChild) {
+      divErr.removeChild(divErr.lastChild);
+      }
+      divErr.appendChild(errPara);
+
       if ($this.hasClass("valid")) {
         $this.removeClass("valid");
       };
   } else{
     $this.addClass("valid");
 
-    var element = document.getElementById("errNom");
-    element.removeChild(errPara);
+    var divErr = document.getElementById("errNom");
+    divErr.removeChild(divErr.lastChild);
+
     if ($this.hasClass("noValid")) {
         $this.removeClass("noValid");
       };
@@ -41,22 +52,35 @@ $(document).ready(function(){
 
    cognom.on("blur",function(){
     var $this = $(this);
-    var re = /^[A-Z a-z]{3,20}$/;
-    var val = $this.val();
+    var regex = /^[A-Z a-z]{3,20}$/;
+    var valor = $this.val();
 
-    if(!re.test(val)){
+    var errPara = document.createElement("p");
+    var errText = document.createTextNode("Mínim 3 caràcters i màxim 30 caràcters");
+    errPara.appendChild(errText);
+
+    if(!regex.test(valor)){
+      $this.addClass("noValid");
+      var divErr = document.getElementById("errCognom");
+      if (divErr.firstChild) {
+      divErr.removeChild(divErr.lastChild);
+      }
+      divErr.appendChild(errPara);
+
       if ($this.hasClass("valid")) {
         $this.removeClass("valid");
       };
-      $this.addClass("noValid");
   } else{
     $this.addClass("valid");
+
+    var divErr = document.getElementById("errCognom");
+    divErr.removeChild(divErr.lastChild);
+
     if ($this.hasClass("noValid")) {
         $this.removeClass("noValid");
       };
   }
 });
-
 
     //Comprova si el EMAIL esta buit
     email.on("blur",function(){
@@ -87,53 +111,38 @@ $(document).ready(function(){
   });
 
 
-    //clear inputs on click
-    $(inp).on("focus",function(){
-      if ($this.hasClass("noValid")) {
-        $this.removeClass("noValid");
-      };
-
-      if ($this.hasClass("valid")) {
-        $this.removeClass("valid");
-      };
-
-    });
     
     //show modal when inputs are valid and button
     //is clicked
-    $(".submit").on("click",function(){
-      if(name.val() !== "Name" && name.val() !== "" &&
-       email.val() !== "Email" && 
-       email.val() !== "" &&
-       comment.val() !== "Comments" && 
-       comment.val()!== ""){
+    // $(".submit").on("click",function(){
+    //   if(name.val() !== "Name" && name.val() !== "" &&
+    //    email.val() !== "Email" && 
+    //    email.val() !== "" &&
+    //    comment.val() !== "Comments" && 
+    //    comment.val()!== ""){
 
-          //remove invalid/valid classes
-        name.removeClass().addClass("input");
-        email.removeClass().addClass("input");
-        comment.removeClass().addClass("input");
+    //       //remove invalid/valid classes
+    //     name.removeClass().addClass("input");
+    //     email.removeClass().addClass("input");
+    //     comment.removeClass().addClass("input");
 
-          //modal box
-          modal_name.text(name.val());
-          modal.slideDown("medium")
-          .delay(2000).slideUp("fast");
+    //       //modal box
+    //       modal_name.text(name.val());
+    //       modal.slideDown("medium")
+    //       .delay(2000).slideUp("fast");
 
-          //put default text back
-          name.val("Name");
-          email.val("Email");
-          comment.val("Comments");
+    //       //put default text back
+    //       name.val("Name");
+    //       email.val("Email");
+    //       comment.val("Comments");
 
-          return false;
+    //       return false;
 
-        } else{
+    //     } else{
 
-          return false; }
-        });
+    //       return false; }
+    //     });
 
-    $.datepicker.formatDate( "dd-mm-yy", new Date( 2007, 7 - 1, 26 ) );
-    
-    $(function() {
-    $( "#datepicker" ).datepicker();
-  });
+
     
   });
