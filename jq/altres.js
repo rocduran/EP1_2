8b7283a94bccf0d2939e9a2803acd1a19406254a
sesiomterim <sesiom_and@hotmail.com>
2015-04-18 19:26:42 +0200

    indexedDB fet, queda a fer modificacions i esborrat (pilla h2 i segons sigui modificarXML o modificarJSON)

--- jq/altres.js
+++ jq/altres.js
@@ -1,22 +1,32 @@
 window.onload = function() {
+	//indexedDB.deleteDatabase("RMBD");
 	associarEvents();
 };
-	
+
 function associarEvents() {
 	xmlFolder = '/RocMoi/BD/XMLDPEspanya2015.xml';
 	jsonFolder = '/RocMoi/BD/XMLDPFrança2015.txt';
 	contingut = document.getElementById("contingutFitxer");
+	titulCont = document.getElementById("titulH");
+	crud = document.getElementById("crud");
+	amagarCrud();
 
-	document.getElementById("jsonBut").onclick = carregarJSON; 
-	document.getElementById("xmlBut").onclick = carregarXML; 
+
+	document.getElementById("jsonBut").onclick = presentarJSON;
+	document.getElementById("xmlBut").onclick = presentarXML;
+	//document.getElementById("view").onclick = ;
+	
+	crearDB();
+	volcarXML();
+	volcarJSON();
 }
 
-function crearObjecteAjax(){
+function crearObjecteAjax() {
 	var obj;
-	if(window.XMLHttpRequest){ //si no es IE
+	if (window.XMLHttpRequest) { //si no es IE
 		obj = new XMLHttpRequest();
 	} else { //es IE o no te objecte
-		try{
+		try {
 			obj = new ActiveXObject("Microsoft.XMLHTTP");
 		} catch (e) {
 			alert("Navegador utilitzat no soportat !")
@@ -25,71 +35,176 @@ function crearObjecteAjax(){
 	return obj;
 }
 
-function carregarJSON(){
-	jsonRequest = crearObjecteAjax();
-	jsonRequest.open('GET',jsonFolder,true);
-	jsonRequest.send("");
-	jsonRequest.onreadystatechange = function()
-	{
-		if(jsonRequest.readyState == 4 && jsonRequest.status == 200){
-			contingut.innerHTML = "";
-			txt="" // txt comtindra el codi html a inserir :D
-			doc = JSON.parse(jsonRequest.responseText);
-		    for (i=0;i<doc.length;i++){
-	    		txt = txt + "<p class=\"item\">";
+function crearDB() {
+	alert("IDB suportat!");
+	var idbSupported = false;
 
-	    		txt = txt + "Codi: " + doc[i].CODI + "<BR>";
-
-	    		txt = txt + "Tipus: " + doc[i].TIPUS + "<BR>";
-
-	    		txt = txt + "Pregunta: " + doc[i].PREGUNTA + "<BR>";
-
-	    		txt = txt + "Data Inici: " + doc[i].DATAI + "<BR>";
-
-	    		txt = txt + "Data Final: " + doc[i].DATAF;
-
-	    		txt = txt + "</p>";
-	   		}	
-		    contingut.innerHTML = txt;
-		}
+	if ("indexedDB" in window) {
+		idbSupported = true;
 	}
-	alert("JSON carregat! :D");
+	if (idbSupported) {
+		var openRequest = indexedDB.open("RMBD", 1);
+
+		openRequest.onupgradeneeded = function(e) {
+			var rmdb = e.target.result;
+
+			if (!rmdb.objectStoreNames.contains("itemsXML")) {
+				rmdb.createObjectStore("itemsXML"); //storage per fitxer xml
+			}
+
+			if (!rmdb.objectStoreNames.contains("itemsJSON")) {
+				rmdb.createObjectStore("itemsJSON"); //storage per fitxer json
+			}
+		}
+		openRequest.onsuccess = function(e) {
+			console.log("Success!");
+			idb = e.target.result;
+		}
+		openRequest.onerror = function(e) {
+			console.log("Error");
+			console.dir(e);
+		}
+	} 
+
 }
 
-function carregarXML() {
-	xmlRequest = crearObjecteAjax();
-	xmlRequest.open('GET',xmlFolder, true);
+function volcarJSON(){
+	jsonRequest = crearObjecteAjax();
+	jsonRequest.open('GET', jsonFolder, true);
+	jsonRequest.send("");
+	jsonRequest.onreadystatechange = function() {
+		if (jsonRequest.readyState == 4 && jsonRequest.status == 200) {
+
+			var transaction = idb.transaction(["itemsJSON"],"readwrite").objectStore("itemsJSON");
+			
+			doc = JSON.parse(jsonRequest.responseText);
+
+			for (var i = 0; i < doc.length; i++) {
+				var objecte = new Object;
+
+				objecte.Codi = doc[i].CODI;
+
+				objecte.Tipus = doc[i].TIPUS;
+
+				objecte.Pregunta = doc[i].PREGUNTA;
+
+				objecte.DataI = doc[i].DATAI;
+
+				objecte.DataF = doc[i].DATAF;
+
+				var request = transaction.add(objecte,i);
+
+			}
+
+			request.onerror =function(e){
+				alert("El fitxer ja s'havia carregat", e.target.error.name);
+			}
+			request.onsuccess = function(e){
+				alert("Fitxer carregat amb exit");
+			}
+		}
+	}
+}
+
+function presentarJSON(){
+	var objectStore = idb.transaction("itemsJSON").objectStore("itemsJSON");
+	txt=""; //html a presentar
+
+	objectStore.openCursor().onsuccess = function(e){
+		var cursor = e.target.result;
+		if (cursor){
+			txt=txt+"<p class=\"item\">Codi: "+cursor.value.Codi+"<BR>";
+			txt=txt+"Tipus: "+cursor.value.Tipus+"<BR>";
+			txt=txt+"Pregunta: "+cursor.value.Pregunta+"<BR>";
+			txt=txt+"Data Inici: "+cursor.value.DataI+"<BR>";
+			txt=txt+"Data Fi: "+cursor.value.DataF+"<BR>";
+			txt=txt+"</p>"
+
+			cursor.continue();
+		}
+		contingut.innerHTML =txt;
+		titulCont.innerHTML ="Contingut democràcia participativa a França:";
+		mostrarCrud();
+	}
+}
+
+function volcarXML(){
+	var xmlRequest = crearObjecteAjax();
+	xmlRequest.open('GET', xmlFolder, true);
 	xmlRequest.send("");
-	xmlRequest.onreadystatechange = function()
-	{
-		if(xmlRequest.readyState == 4 && xmlRequest.status == 200){
-			contingut.innerHTML = "";
-			txt="";
-			doc= xmlRequest.responseXML.documentElement.getElementsByTagName("ITEM");
-			for (i=0; i<doc.length; i++){
-				txt = txt + "<p class=\"item\">";
+
+	xmlRequest.onreadystatechange = function() {
+		if (xmlRequest.readyState == 4 && xmlRequest.status == 200) {	
+
+			var transaction = idb.transaction(["itemsXML"],"readwrite").objectStore("itemsXML");
+
+			var doc = xmlRequest.responseXML.documentElement.getElementsByTagName("ITEM");
+
+			for (var i = 0; i < doc.length; i++) {
+				var objecte = new Object;
+
 				aux = doc[i].getElementsByTagName("CODI");
-				txt = txt + "Codi: " + aux[0].firstChild.nodeValue + "<BR>";
+				objecte.Codi = aux[0].firstChild.nodeValue;
 
 				aux = doc[i].getElementsByTagName("TIPUS");
-				txt = txt + "Tipus: " + aux[0].firstChild.nodeValue + "<BR>";
+				objecte.Tipus = aux[0].firstChild.nodeValue;
 
 				aux = doc[i].getElementsByTagName("PREGUNTA");
-				txt = txt + "Pregunta: " + aux[0].firstChild.nodeValue + "<BR>";
+				objecte.Pregunta = aux[0].firstChild.nodeValue;
 
 				aux = doc[i].getElementsByTagName("DATAI");
-				txt = txt + "Data Inici: " + aux[0].firstChild.nodeValue + "<BR>";
+				objecte.DataI = aux[0].firstChild.nodeValue;
 
 				aux = doc[i].getElementsByTagName("DATAF");
-				txt = txt + "Data Fi: " + aux[0].firstChild.nodeValue;
+				objecte.DataF = aux[0].firstChild.nodeValue;
 
-				txt = txt + "</p>";
-	    	}
-	   		contingut.innerHTML = txt;	
+				var request = transaction.add(objecte,i);
+
+			}
+
+			request.onerror =function(e){
+				alert("El fitxer ja s'havia carregat", e.target.error.name);
+			}
+
+			request.onsuccess = function(e){
+				alert("Fitxer carregat amb exit");
+			}
 		}
 	}
-	alert("XML carregat! :D");	
 }
+
+function presentarXML(){
+	var objectStore = idb.transaction("itemsXML").objectStore("itemsXML");
+	txt=""; //html a presentar
+
+	objectStore.openCursor().onsuccess = function(e){
+		var cursor = e.target.result;
+		if (cursor){
+			txt=txt+"<p class=\"item\">Codi: "+cursor.value.Codi+"<BR>";
+			txt=txt+"Tipus: "+cursor.value.Tipus+"<BR>";
+			txt=txt+"Pregunta: "+cursor.value.Pregunta+"<BR>";
+			txt=txt+"Data Inici: "+cursor.value.DataI+"<BR>";
+			txt=txt+"Data Fi: "+cursor.value.DataF+"<BR>";
+			txt=txt+"</p>"
+
+			cursor.continue();
+		}
+		contingut.innerHTML =txt;
+		titulCont.innerHTML ="Contingut democràcia participativa a Espanya:";
+		mostrarCrud();
+	}
+	
+}
+
+function amagarCrud(){
+	crud.style.display="none";
+}
+
+function mostrarCrud(){
+	crud.style.display="block";
+}
+
+
 
 
 
