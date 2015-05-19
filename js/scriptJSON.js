bc81136671252c8d6c22d7fb730bf063e94668b5
sesiomterim <sesiom_and@hotmail.com>
2015-05-19 21:00:09 +0200

    Part BD admin començada, de moment lliste les noticies de la BD i si clickes a la basura les elimina correctament :3 XDD
    S'han de remplaçar tots els div class container per : div id containerNoticies etc sino pete ( lo que hem comentat aquest mati)
    
    Primer de tot faria lo dels ID per que cuadres amb el CSS i despres la resta. pa no liarla XD (no querias commits ? pos tomaaa jajajajaja XP)
    
     @rocduran

--- js/scriptJSON.js
+++ js/scriptJSON.js
@@ -2,20 +2,22 @@ $(document).ready(init);
 //Inicialitzar associacions Events-formulari
 //Altres inits 
 function init(){
-associaEvents();
-altres();
+	associaEvents();
+	altres();
 }
+
 //Connector Events--Formulari
 function associaEvents(){
-$("#actualizarDades").click(llistatNoticies);
+	$("a.noticies").click(llistatNoticies);
 }
+
 //Altres inits
 function altres(){};
 
 //Peticó AJAX (llistat total de clients de la BD)
 function llistatNoticies(dades){
-    $.ajax
-	({	url: 'llistatComplert.php',
+	$.ajax
+	({	url: 'php/LlistatNoticies.php',
 		dataType: 'json',
 		type: 'get',
 		cache: false, //IE per a defecte emmagatzema en caché (evitar-ho-->false)
@@ -24,12 +26,12 @@ function llistatNoticies(dades){
 	});
 }
 
-function f2(valor) {
+function eliminaNoticia(valor) {
 //alert(valor);
 $.ajax
-	({	url: 'elimina.php?id='+valor,
-		dataType: 'json',
-		type: 'get',
+({	url: 'php/eliminaNoticia.php?id='+valor,
+	dataType: 'json',
+	type: 'get',
 		cache: false, //IE per a defecte emmagatzema en caché (evitar-ho-->false)
 		//Trucada a funció (per no programar-la aquí i respectar el model)
 		//Decidim que després d'eliminar tornem a presentar les dades
@@ -38,26 +40,27 @@ $.ajax
 }	
 //Funció de presentació quan es rebin les dades formatades
 function presentaDades(data){
-	$("#resultats").html("");
-	$("#resultats").append(
+	$("#containerNoticies").html("");
+	$("#containerNoticies").append(
 		$.each(data, function(index){
 			var id = data[index].id;
-			var tipus = data[index].tipus;
 			var titol = data[index].titol;
-			var contingut = data[index].contingut;
-			var url=data[index].url;
-			$("#resultats").append('<p>Id    : '+id+'</p>');
-			//Proposta 1. Crear enllaços dinàmics amb href 
-			// $("#resultats").append('<a href=modificar.php?id='+id+'>Editar | ');
-			// $("#resultats").append('<a href=eliminar.php?id='+id+'>Eliminar');
-			//Proposta 2. Crear botó o imatge que invoqui una funció amb paràmetre
-			$cadena="<button name='"+ id + "' onclick='f2("+id +");'>Eliminar</button>";
-			$("#resultats").append($cadena);
-			$("#resultats").append('<p>Titol    : '+titol+'\t <br> Contingut  :'+contingut+'\t <br> Url : '+url+'</p><br>');
-			$("#resultats").append('<img class="grafic" src="../' +url+'" height="100%" width="100%" border=0 alt="">');
- 			
+
+			var txt = '';
+			txt = txt +'<div class="item">';
+			txt = txt +'<div class="textitem" >';
+			txt = txt +'<span>'+titol+'</span>';
+			txt = txt +'</div>';
+			txt = txt +'<div class="botons">';
+			txt = txt +'<span><img src="img/pencil.png" alt="edita"> </span>';
+			txt = txt +'<span><img src="img/bin.png" onclick="eliminaNoticia('+id +')" alt="borrar"> </span>';
+			txt = txt +'</div>';
+			txt = txt +'</div>';
+
+			$("#containerNoticies").append(txt);
+
 		})
-	);
+);
 }
 
- 
+
