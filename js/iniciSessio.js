3fb9c5ae121c752dac9a329aac20f2f26c56e0cd
sesiomterim <sesiom_and@hotmail.com>
2015-05-31 15:50:03 +0200

    Part autentificacio LDAP acabada, es pot accedir a la web amb el login de la uni i el nom d'usuari es guarde en una variable de sessio que serveix per presentarlo on avans hi havie "Roc Duran". He hagut de modificar toots els html xq canviabe la part del menu de navegació. Nomes quede la part 3 i boats and bitches !! jajajaja @rocduran

--- js/iniciSessio.js
+++ js/iniciSessio.js
@@ -2,12 +2,14 @@ $(document).ready(init);
 //Inicialitzar associacions Events-formulari
 function init(){
 	associaEvents();
+	determinaVariableSessio();
 }
 
 //Connector Events
 function associaEvents(){
 	// MENU VERTICAL//
 	$("#BotoConectat").click(conecta);
+	$("#sortir").click(desconecta);
 }
 
 function conecta(){
@@ -18,7 +20,7 @@ function conecta(){
 	$.ajax
 	({	url: 'php/conectat.php',
 		type: 'POST',
-		data: 'usuari='+usuari+'&contrasenya='+contrasenya,
+		data: 'quin=login&usuari='+usuari+'&contrasenya='+contrasenya,
 		cache: false, //IE per a defecte emmagatzema en caché (evitar-ho-->false)
 		//Trucada a funció (per no programar-la aquí i respectar el model)
 		success: function(data) {resposta(data, usuari);} 
@@ -26,11 +28,47 @@ function conecta(){
 }
 
 function resposta(estat, usuari){
-	if(estat == 1){
+	if(estat =="true"){
 		alert("BENVINGUT ! " + usuari);
-
-		
+		determinaVariableSessio();
+		location.reload();
 	} else {
 		alert("NOM D'USUARI O CONTRASENYA INCORRECTA !");
+	}
+}
+
+function desconecta(){
+	$.ajax
+	({	url: 'php/conectat.php',
+		type: 'POST',
+		data: 'quin=logout',
+		cache: false, //IE per a defecte emmagatzema en caché (evitar-ho-->false)
+		//Trucada a funció (per no programar-la aquí i respectar el model)
+		success: function() {determinaVariableSessio();} 
+	});
+	location.reload();
+}
+
+function determinaVariableSessio(){
+	$.ajax
+	({	url: 'php/conectat.php',
+		type: 'POST',
+		data: 'quin=load',
+		cache: false, //IE per a defecte emmagatzema en caché (evitar-ho-->false)
+		//Trucada a funció (per no programar-la aquí i respectar el model)
+		success: function(data) {presentacio(data);} 
+	});
+}
+
+function presentacio(data){
+	if(data[0]){
+		document.getElementById("noConectat").style.display = "none";
+		document.getElementById("conectat").style.display = "hidden";
+		document.getElementById("nomUsuari").text = data[1];
+
+	} else {
+		document.getElementById("nomUsuari").text = "Inicia Sessió";
+		document.getElementById("noConectat").style.display = "hidden";
+		document.getElementById("conectat").style.display = "none";
 	}
 }
--- php/conectat.php
+++ php/conectat.php
@@ -1,13 +1,33 @@
 <?php
+session_start();
+switch ($_POST["quin"]){
+	case "login":
+		$ldap = ldap_connect("ldap.uda.ad",389) or die("No hi ha manera de trobar LDAP");
 
-$ldap = ldap_connect("ldap.uda.ad",389) or die("No hi ha manera de trobar LDAP");
+		if ($bind = ldap_bind($ldap, "mail=".$_POST["usuari"]."@uda.ad", $_POST["contrasenya"])) {
+			$_SESSION['usuari']  = $_POST["usuari"]."@uda.ad";
+			$ok = "true";
+		} else {
+			$ok = "false";
+		}
+		header('Content-type: application/json');
+		echo json_encode($ok);
+	break;
 
+	case "load":
+		if ($_SESSION['usuari']){ //Si existeix la variable de sessio 'usuari
+			$ok[0] = true;
+			$ok[1] = $_SESSION['usuari'];
+		} else {
+			$ok[0] = false;
+			$ok[1] = null;
+		}
+		header('Content-type: application/json');
+		echo json_encode($ok);
+	break;
 
-if ($bind = ldap_bind($ldap, "mail=".$_POST["usuari"]."@uda.ad", $_POST["contrasenya"])) {
-	$_SESSION['nomUsuari'] = $_POST["usuari"]."@uda.ad";
-	echo 1;
-} else {
-	echo 2;
+	case "logout":
+		session_destroy();
+	break;
 }
-
 ?>
